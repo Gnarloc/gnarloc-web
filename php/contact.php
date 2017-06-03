708f017cde81cf65b8eef3af6a70c97feb3366eb
Garrett Schultz <gratschultz2013@hotmail.com>
2017-06-02 22:18:41 -0500

    updated footer styling and about section

---
+++ php/contact.php
@@ -0,0 +1,31 @@
+<?php
+if($_REQUEST['first_name'] == '' || $_REQUEST['contact_email'] == '' ||  $_REQUEST['message'] == ''):
+  return "error";
+endif;
+if (filter_var($_REQUEST['contact_email'], FILTER_VALIDATE_EMAIL)):
+  $subject = 'New Email from GnarLock Web Contact Form'; // Subject of your email
+
+  // Receiver email address
+  $to = 'info@gnarloc.com';  //send-to GnarLoc info email
+ 
+
+  // prepare header
+  $header = 'From: '. $_REQUEST['first_name'] . " " .$_REQUEST['last_name'] . ' <'. $_REQUEST['contact_email'] .'>'. "\r\n";
+  $header .= 'Reply-To:  '. $_REQUEST['first_name'] . " " .$_REQUEST['last_name'] . ' <'. $_REQUEST['contact_email'] .'>'. "\r\n";
+  $header .= 'X-Mailer: PHP/' . phpversion();
+
+
+  $message .= 'Name: ' . $_REQUEST['first_name'] . " " .$_REQUEST['last_name'] . "\n";
+  $message .= 'Email: ' . $_REQUEST['contact_email'] . "\n";
+  $message .= 'Subject: ' . $_REQUEST['contact_subject'] . "\n";
+  $message .= 'Message: '. $_REQUEST['message'];
+
+  // Send contact information
+  $mail = mail( $to, $subject , $message, $header );
+
+  echo 'sent';
+  else:
+    return "error";
+  endif;
+
+?>
